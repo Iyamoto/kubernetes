23a4fc68ef30b88ee075ffcfdbfd2c8b04b1caf8
iyamoto <iyamoto@gmail.com>
2017-11-18 14:42:50 +0300

    kubectl and minikube added

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,5 +8,5 @@ sudo pacman -Syu
 sudo pacman -S --noconfirm puppet
 sudo pacman -Sc --noconfirm
 
-# sudo puppet apply /vagrant/puppet/bootstrap.pp
-# sudo puppet apply /vagrant/puppet/k8s.pp
+sudo puppet apply /vagrant/puppet/bootstrap.pp
+sudo puppet apply /vagrant/puppet/k8s.pp
